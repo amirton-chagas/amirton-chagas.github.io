00fc627fca668a0dcdda1373438fe2096b79aaf1
abc@cin.ufpe.br <chagas@amirton.com>
2020-06-06 21:23:08 -0300

    - version with rsvp and better responsiveness

--- lorena/js/scripts.js
+++ lorena/js/scripts.js
@@ -759,9 +759,21 @@ function init() {
         marker.setMap(map);
     }
 }
-if (google !== undefined){
-    google.maps.event.addDomListener(window, 'load', init);
+//if (google !== undefined){
+//    google.maps.event.addDomListener(window, 'load', init);
+//}
+
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
 }
+
+docReady(init);
 /* Modernizr 2.6.2 (Custom Build) | MIT & BSD
  * Build: http://modernizr.com/download/#-fontface-backgroundsize-borderimage-borderradius-boxshadow-flexbox-hsla-multiplebgs-opacity-rgba-textshadow-cssanimations-csscolumns-generatedcontent-cssgradients-cssreflections-csstransforms-csstransforms3d-csstransitions-applicationcache-canvas-canvastext-draganddrop-hashchange-history-audio-video-indexeddb-input-inputtypes-localstorage-postmessage-sessionstorage-websockets-websqldatabase-webworkers-geolocation-inlinesvg-smil-svg-svgclippaths-touch-webgl-shiv-mq-cssclasses-addtest-prefixed-teststyles-testprop-testallprops-hasevent-prefixes-domprefixes-load
  */
