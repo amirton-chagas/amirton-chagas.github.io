81a75dac818d2e648eeccfe3846d5d8241087b0a
abc@cin.ufpe.br <chagas@amirton.com>
2020-11-07 11:23:09 -0300

    - updating gifts and date

--- lorena/js/scripts.js
+++ lorena/js/scripts.js
@@ -917,7 +917,7 @@ docReady(init);
 	};
 
 	// Set the date we're counting down to
-	var countDownDate = new Date("Nov 14, 2020 15:00:00").getTime();
+	var countDownDate = new Date("Feb 27, 2021 15:00:00").getTime();
 
 	// Update the count down every 1 second
 	var x = setInterval(function() {
